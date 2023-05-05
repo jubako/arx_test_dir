96c6e88bc7d30a1ab5629f7ec62c94498d6c485c
Matthieu Gautier <mgautier@kymeria.fr>
2023-05-05 12:07:17 +0200

    Create a tree structure first and then generate.

--- src/main.rs
+++ src/main.rs
@@ -3,71 +3,48 @@ use rand::prelude::*;
 use std::cell::RefCell;
 use std::fs::create_dir;
 use std::io::{Read, Result};
-use std::ops::Range;
+use std::ops::{DerefMut, Range};
 use std::path::{Path, PathBuf};
 use std::rc::Rc;
 
 use clap::Parser;
 
-struct BinRead {
-    rng: SmallRng,
-    len: usize,
-}
+struct BinRead(SmallRng);
 
 impl BinRead {
-    fn new(seed: u64, len: usize) -> Self {
-        Self {
-            rng: SmallRng::seed_from_u64(seed),
-            len,
-        }
+    fn new(rng: SmallRng) -> Self {
+        Self(rng)
     }
 }
 
 impl Read for BinRead {
     fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
-        let to_read_len = std::cmp::min(buf.len(), self.len);
-        self.rng.fill_bytes(&mut buf[..to_read_len]);
-        self.len -= to_read_len;
+        let to_read_len = std::cmp::min(buf.len(), 1024);
+        self.0.fill_bytes(&mut buf[..to_read_len]);
         Ok(to_read_len)
     }
 }
 
-struct TextRead {
-    rng: SmallRng,
-    len: usize,
-}
+struct TextRead(SmallRng);
 
 impl TextRead {
-    fn new(seed: u64, len: usize) -> Self {
-        Self {
-            rng: SmallRng::seed_from_u64(seed),
-            len,
-        }
+    fn new(rng: SmallRng) -> Self {
+        Self(rng)
     }
 }
 
 impl Read for TextRead {
     fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
         // let's say that means words len is height
-        let mut words_to_generate = std::cmp::min(buf.len() >> 3, self.len);
-        let string = lipsum::lipsum_words_with_rng(&mut self.rng, words_to_generate);
-        let mut source_len = string.len();
-        loop {
-            match string[0..source_len].rfind(' ') {
-                None => break,
-                Some(i) => {
-                    source_len = i;
-                    words_to_generate -= 1;
-                    if i < buf.len() {
-                        break;
-                    }
-                }
-            }
-        }
+        let string = lipsum::lipsum_words_with_rng(&mut self.0, 1024);
+        let source_len = std::cmp::min(buf.len(), string.len());
         buf[0..source_len].copy_from_slice(string[0..source_len].as_bytes());
-        self.len -= words_to_generate;
         Ok(source_len)
     }
+}
+
+fn name(rng: &mut SmallRng) -> String {
+    (0..7).map(|_| rng.sample(Alphanumeric) as char).collect()
 }
 
 #[derive(Debug)]
@@ -105,9 +82,7 @@ impl Context {
     }
 
     fn name(&self) -> String {
-        (0..7)
-            .map(|_| self.rng.borrow_mut().sample(Alphanumeric) as char)
-            .collect()
+        name(self.rng.borrow_mut().deref_mut())
     }
 
     fn descent(&self) -> Self {
@@ -124,12 +99,16 @@ impl Context {
         }
     }
 
-    fn binary_read(&self, len: usize) -> BinRead {
-        BinRead::new(self.rng.borrow_mut().gen(), len)
+    fn rng(&self) -> SmallRng {
+        SmallRng::seed_from_u64(self.rng.borrow_mut().gen())
     }
 
-    fn text_read(&self, len: usize) -> TextRead {
-        TextRead::new(self.rng.borrow_mut().gen(), len)
+    fn binary_read(&self) -> BinRead {
+        BinRead::new(self.rng())
+    }
+
+    fn text_read(&self) -> TextRead {
+        TextRead::new(self.rng())
     }
 }
 
@@ -195,51 +174,108 @@ impl ContextBuilder {
         }
     }
 }
-fn build_random_file(path: &Path, context: &Context) -> Result<()> {
-    let mut file = std::fs::OpenOptions::new()
-        .write(true)
-        .create_new(true)
-        .open(path)?;
-    let len = context.file_len();
-    let mut random = context.binary_read(len);
-    std::io::copy(&mut random, &mut file)?;
-    Ok(())
+
+struct FileEntry {
+    path: PathBuf,
+    source: Box<RefCell<dyn Read>>,
+    size: usize,
 }
 
-fn build_text_file(path: &Path, context: &Context) -> Result<()> {
-    let mut file = std::fs::OpenOptions::new()
-        .write(true)
-        .create_new(true)
-        .open(path)?;
-    let len = context.text_len();
-    let mut random = context.text_read(len);
-    let size = std::io::copy(&mut random, &mut file)?;
-    assert!(size <= context.file_len.end as u64);
-    Ok(())
+impl FileEntry {
+    fn new(path: PathBuf, source: Box<RefCell<dyn Read>>, size: usize) -> Self {
+        Self { path, source, size }
+    }
+
+    fn generate(&self) -> Result<()> {
+        let mut file = std::fs::OpenOptions::new()
+            .write(true)
+            .create_new(true)
+            .open(&self.path)?;
+        std::io::copy(
+            &mut self.source.borrow_mut().deref_mut().take(self.size as u64),
+            &mut file,
+        )?;
+        Ok(())
+    }
+
+    fn size(&self) -> usize {
+        self.size
+    }
+}
+
+struct DirEntry {
+    path: PathBuf,
+    files: Vec<FileEntry>,
+    dirs: Vec<DirEntry>,
+}
+
+impl DirEntry {
+    fn new(mut path: PathBuf, context: Context) -> Self {
+        let (nb_files, nb_dir) = context.nb_child();
+        let dirs = (0..nb_dir)
+            .map(|_| {
+                let child_name: PathBuf = context.name().into();
+                path.push(child_name);
+                let d = DirEntry::new(path.clone(), context.descent());
+                path.pop();
+                d
+            })
+            .collect();
+        let files = (0..nb_files)
+            .map(|_| {
+                let child_name: PathBuf = context.name().into();
+                path.push(child_name);
+                let f = if context.is_binary() {
+                    path.set_extension("bin");
+                    FileEntry::new(
+                        path.clone(),
+                        Box::new(RefCell::new(context.binary_read())),
+                        context.file_len(),
+                    )
+                } else {
+                    path.set_extension("text");
+                    FileEntry::new(
+                        path.clone(),
+                        Box::new(RefCell::new(context.text_read())),
+                        context.text_len(),
+                    )
+                };
+                path.pop();
+                f
+            })
+            .collect();
+        Self { path, files, dirs }
+    }
+
+    fn generate(&self) -> Result<()> {
+        create_dir(&self.path)?;
+        for dir in &self.dirs {
+            dir.generate()?;
+        }
+        for file in &self.files {
+            file.generate()?;
+        }
+        Ok(())
+    }
+
+    fn nb_files(&self) -> u64 {
+        let nb_files = self.files.len() as u64;
+        nb_files + self.dirs.iter().map(|d| d.nb_files()).sum::<u64>()
+    }
+
+    fn size(&self) -> usize {
+        let file_size = self.files.iter().map(|f| f.size()).sum::<usize>();
+        let dir_size = self.dirs.iter().map(|d| d.size()).sum::<usize>();
+        file_size + dir_size
+    }
 }
 
 fn build_dir(path: &Path, context: Context) -> Result<()> {
-    create_dir(path)?;
-    let mut path: PathBuf = path.into();
-    let (nb_files, nb_dir) = context.nb_child();
-    for _i in 0..nb_dir {
-        let child_name: PathBuf = context.name().into();
-        path.push(child_name);
-        build_dir(&path, context.descent())?;
-        path.pop();
-    }
-    for _i in 0..nb_files {
-        let child_name: PathBuf = context.name().into();
-        path.push(child_name);
-        if context.is_binary() {
-            path.set_extension("bin");
-            build_random_file(&path, &context)?;
-        } else {
-            path.set_extension("text");
-            build_text_file(&path, &context)?;
-        }
-        path.pop();
-    }
+    let dir = DirEntry::new(path.into(), context);
+    let nb_files = dir.nb_files();
+    let size = dir.size();
+    println!("Generate {nb_files} files for a {size} bytes.");
+    dir.generate()?;
     Ok(())
 }
 
